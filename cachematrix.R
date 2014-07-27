370aae4760072c7b2207186a27be77a237a4df39
Shalvindra Prasad <shalvindra@outlook.com>
2014-07-28 11:09:37 +1200

    Wrote body of the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions calculate and store inverse of a matrix in cache
 
-## Write a short comment describing this function
-
+## Function creates a special vector 'matrix'
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    
+    get<-function() x
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+    
+    list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Function checks to see if an inverse of the matrix exists in the cache
+## Else, calculates and stores the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    
+    ## Return a matrix that is the inverse of 'x'
+    m
 }
